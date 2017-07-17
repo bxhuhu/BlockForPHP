a77422683116da04b6fb04c42d529c9eb8ac01c8
陆勇江 <1091064779@qq.com>
2017-07-17 16:42:30 +0800

    添加上传视频模块

--- Phone/Controller/VideoController.class.php
+++ Phone/Controller/VideoController.class.php
@@ -18,4 +18,29 @@ class VideoController extends RestController
         }
     }
 
+    public function video_new()
+    {
+        if (IS_POST) {
+            $file = new FileController();
+            $path = $file->saveBigFile();
+            if (sizeof($path) > 0) {
+                $model = D('video');
+                $model->url = $path['videoPath'];
+                $model->title = I('title');
+                $model->create_data = date("Y-m-d h:m:s");
+                $model->img = $path['imgPath'];
+
+                if (I('title') == null || $path['imgPath'] == null || $path['videoPath'] == null) {
+                    $this->response(\DataFormat::failFormat("视频和标题以及图片不能为空"), 'json');
+                }
+                $model->add();
+                $this->response(\DataFormat::successFormat("视频上传成功", $model), 'json');
+            } else {
+                $this->response(\DataFormat::failFormat("添加视频失败"), 'json');
+            }
+            exit;
+        }
+        $this->display();
+    }
+
 }
